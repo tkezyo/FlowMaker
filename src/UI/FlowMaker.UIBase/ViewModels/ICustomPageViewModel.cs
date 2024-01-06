a0a9aee5f9b6aa1ed9a16209cf3b75cb9e0ca9d6
金俊成 <tkezyo@live.com>
2024-01-06 22:56:15 +0800

    拆分文件

---
+++ src/UI/FlowMaker.UIBase/ViewModels/ICustomPageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using FlowMaker.Models;
+using ReactiveUI;
+
+namespace FlowMaker.ViewModels
+{
+    public interface ICustomPageViewModel : ICustomPageInjectViewModel, IRoutableViewModel
+    {
+        /// <summary>
+        /// 类别
+        /// </summary>
+        static abstract string Category { get; }
+        /// <summary>
+        /// 名称
+        /// </summary>
+        static abstract string Name { get; }
+        static abstract CustomViewDefinition GetDefinition();
+        Task WrapAsync(List<FlowInput> inputs, IServiceProvider serviceProvider, CancellationToken cancellationToken);
+        Task Load();
+    }
+    public interface ICustomPageInjectViewModel
+    {
+    }
+}
