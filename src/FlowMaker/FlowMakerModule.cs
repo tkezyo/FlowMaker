ce9474026bbc50c277d528bfbe1b47ff0e278108
金俊成 <tkezyo@live.com>
2024-09-26 21:49:47 +0800

    升级包

--- src/FlowMaker/FlowMakerModule.cs
+++ src/FlowMaker/FlowMakerModule.cs
@@ -13,12 +13,12 @@ public class FlowMakerModule : Ty.ModuleBase
         hostApplicationBuilder.Services.AddSingleton<FlowManager>();
         hostApplicationBuilder.Services.AddSingleton<IFlowProvider, FileFlowProvider>();
 
-        hostApplicationBuilder.Services.AddScopedMiddleware<FlowExecuteMiddleware>(FlowExecuteMiddleware.Name, FlowExecuteMiddleware.Name);
         hostApplicationBuilder.Services.AddScopedMiddleware<FlowStateTrackingMiddleware>(FlowStateTrackingMiddleware.Name, FlowStateTrackingMiddleware.Name);
-        hostApplicationBuilder.Services.AddScopedMiddleware<StepGroupExecuteMiddleware>(StepGroupExecuteMiddleware.Name, StepGroupExecuteMiddleware.Name);
+        hostApplicationBuilder.Services.AddScopedMiddleware<FlowExecuteMiddleware>(FlowExecuteMiddleware.Name, FlowExecuteMiddleware.Name);
         hostApplicationBuilder.Services.AddScopedMiddleware<StepGroupTrackingMiddleware>(StepGroupTrackingMiddleware.Name, StepGroupTrackingMiddleware.Name);
-        hostApplicationBuilder.Services.AddScopedMiddleware<StepExecuteMiddleware>(StepExecuteMiddleware.Name, StepExecuteMiddleware.Name);
+        hostApplicationBuilder.Services.AddScopedMiddleware<StepGroupExecuteMiddleware>(StepGroupExecuteMiddleware.Name, StepGroupExecuteMiddleware.Name);
         hostApplicationBuilder.Services.AddScopedMiddleware<StepTrackingMiddleware>(StepTrackingMiddleware.Name, StepTrackingMiddleware.Name);
+        hostApplicationBuilder.Services.AddScopedMiddleware<StepExecuteMiddleware>(StepExecuteMiddleware.Name, StepExecuteMiddleware.Name);
 
 
         return Task.CompletedTask;
